f7f37a622b1df903c846a3ca11a23a0785de3dd8
erreur401 <erreur401@gmail.com>
2023-03-06 10:50:12 +0100

    menu filter + author + info page added

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_flatpages import FlatPages
 from flask import render_template
 
-
-
 FLATPAGES_EXTENSION = '.md'
 FLATPAGES_AUTO_RELOAD = True
 
@@ -13,6 +11,13 @@ pages = FlatPages(app)
 
 pages.get('foo')
 
+def Liste_cat():
+    articles = (p for p in pages if 'published' in p.meta)
+    catList = []
+    for a in articles:
+        catList.append(a.meta['cat'])
+    catList = list(dict.fromkeys(catList))
+    return catList 
 
 @app.route('/')
 def index():
@@ -20,25 +25,36 @@ def index():
     articles = (p for p in pages if 'published' in p.meta)
     latest = sorted(articles, reverse=True,
                     key=lambda p: p.meta['published'])
-
-    catList = []
-    for a in latest:
-        catList.append(a.meta['cat'])
-    
-    catList = list(dict.fromkeys(catList))
-
+    catList = Liste_cat()
     return render_template('index.html', articles=latest , catList=catList  )
-
-
 
 @app.route('/<path:path>')
 def page(path):
     page = pages.get_or_404(path)
-    return render_template('single.html', page=page)
+    catList = Liste_cat()
+    return render_template('single.html', page=page ,catList=catList )
+
+@app.route('/info')
+def info():
+    page = pages.get_or_404('info')
+    catList = Liste_cat()
+    return render_template('staticpage.html', page=page , catList=catList)
+
+@app.route('/cat/<catname>')
+def catPage(catname):
+    articles = (p for p in pages if 'published' in p.meta and p.meta['cat']==catname )
+    latest = sorted(articles, reverse=True,
+                    key=lambda p: p.meta['published'])
+    catList = Liste_cat()
+    return render_template('index.html', articles=latest , catList=catList  )
 
 
-
-
-
+@app.route('/author/<authorname>')
+def authorPage(authorname):
+    articles = (p for p in pages if 'published' in p.meta and  p.meta['author']==authorname )
+    latest = sorted(articles, reverse=True,
+                    key=lambda p: p.meta['published'])
+    catList = Liste_cat()
+    return render_template('index.html', articles=latest , catList=catList  )
 
 
